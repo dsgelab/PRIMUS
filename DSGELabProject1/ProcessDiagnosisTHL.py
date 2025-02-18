7e4d22e9266206c1d4dd93db2f2220e5baa8e789
ferroteo <ferro.teo98@gmail.com>
2025-02-18 15:06:05 +0200

    fix bugs

--- DSGELabProject1/ProcessDiagnosisTHL.py
+++ DSGELabProject1/ProcessDiagnosisTHL.py
@@ -18,8 +18,8 @@ avohilmo_diagnosis = [file for file in os.listdir(THL_path) if re.search(r'AH_pi
 hilmo_base = [file for file in os.listdir(THL_path) if re.search(r'HILMO\d', file) and '~lock' not in file]
 hilmo_diagnosis = [file for file in os.listdir(THL_path) if re.search(r'H_ICD10_\d', file) and '~lock' not in file]
 
-outpath_avohilmo = f'/media/volume/Projects/DSGELabProject1/processed_avohilmo_{time.strftime('%Y%m%d')}.csv'
-outpath_hilmo = f'/media/volume/Projects/DSGELabProject1/processed_hilmo_{time.strftime('%Y%m%d')}.csv'
+outpath_avohilmo = f"/media/volume/Projects/DSGELabProject1/processed_avohilmo_{time.strftime('%Y%m%d')}.csv"
+outpath_hilmo = f"/media/volume/Projects/DSGELabProject1/processed_hilmo_{time.strftime('%Y%m%d')}.csv"
 
 #### Global variables
 N_CPUs = 5
@@ -36,18 +36,21 @@ def merge_extra_avohilmo(base_data, inpath, outpath):
     dtypes = {'FID': 'str','AVOHILMOID':'int','ICD10':'str'}
     df = pd.read_csv(os.path.join(THL_path, inpath), sep=';', encoding='latin-1', usecols= dtypes.keys(), dtype=dtypes)
     merged_data = pd.merge(base_data, df, on=['FID', 'AVOHILMOID'], how='inner')
-    merged_data = merged_data[['FID', 'FD_HASH_Rekisteröinti', 'AVOHILMOID', 'KAYNTI_ALKOI', 'TAPATURMATYYPPI','ULKOINEN_SYY','ICD10']]
-    merged_data.to_csv(outpath, mode='a', header=False, index=False)
+    if not merged_data.empty:
+        merged_data = merged_data[['FID', 'FD_HASH_Rekisteröinti..numero', 'AVOHILMOID', 'KAYNTI_ALKOI', 'TAPATURMATYYPPI','ULKOINEN_SYY','ICD10']]
+        merged_data.to_csv(outpath, mode='a', header=False, index=False)
 
 def merge_extra_hilmo(base_data, inpath, outpath):
-    dtypes = {'FID': 'str','HILMOID':'int','KOODI':'str','N':'int','KENTTA':str}
+    dtypes = {'FID': 'str','HILMOID':'int','KOODI':'str','N':'int','KENTTA':'str'}
     df = pd.read_csv(os.path.join(THL_path, inpath), sep=';', encoding='latin-1', usecols= dtypes.keys(), dtype=dtypes)
     df = df[(df.N==0) & (df.KENTTA=='PDGO')] # filter only main diagnosis 
     merged_data = pd.merge(base_data, df, on=['FID', 'HILMOID'], how='inner')
-    merged_data = merged_data[['FID', 'FD_HASH_Rekisteröinti', 'HILMOID', 'TUPVA', 'KOODI']]
-    merged_data.to_csv(outpath, mode='a', header=False, index=False)
+    if not merged_data.empty:
+        merged_data = merged_data[['FID', 'FD_HASH_Rekisteröinti..numero', 'HILMOID', 'TUPVA', 'KOODI']]
+        merged_data.to_csv(outpath, mode='a', header=False, index=False)
 
 #### Main:
+pd.DataFrame(columns=['FID', 'FD_HASH_Rekisteröinti..numero', 'AVOHILMOID', 'KAYNTI_ALKOI', 'TAPATURMATYYPPI', 'ULKOINEN_SYY', 'ICD10']).to_csv(outpath_avohilmo, encoding='latin-1', index=False)
 for base_file in avohilmo_base:
     start_time = time.time()
     # 1. open file
@@ -65,15 +68,14 @@ for base_file in avohilmo_base:
     # 3. merge diagnosis info (using AVOHILMOID)
     # - multiprocessing for speed 
     # - append to unique output file
-    pd.DataFrame(columns=['FID', 'FD_HASH_Rekisteröinti', 'AVOHILMOID', 'KAYNTI_ALKOI', 'TAPATURMATYYPPI', 'ULKOINEN_SYY', 'ICD10']).to_csv(outpath_avohilmo, index=False)
     with multiprocessing.Pool(processes=N_CPUs) as pool:
         for extra in avohilmo_diagnosis:
-            pool.apply_async(merge_extra_avohilmo, args=(df, extra, outpath_avohilmo))
+            pool.apply(merge_extra_avohilmo, args=(df, extra, outpath_avohilmo))
     pool.close()
-    pool.join()
     end_time = time.time()
     print(f'Time taken for {base_file}: {end_time - start_time} seconds')
 
+pd.DataFrame(columns=['FID', 'FD_HASH_Rekisteröinti..numero', 'HILMOID', 'TUPVA', 'KOODI']).to_csv(outpath_hilmo, encoding='latin-1', index=False)
 for base_file in hilmo_base:
     start_time = time.time()
     # 1. open file
@@ -89,11 +91,9 @@ for base_file in hilmo_base:
     # 3. merge diagnosis info (using HILMOID)
     # - multiprocessing for speed 
     # - append to unique output file
-    pd.DataFrame(columns=['FID', 'FD_HASH_Rekisteröinti', 'HILMOID', 'TUPVA', 'KOODI']).to_csv(outpath_hilmo, index=False)
     with multiprocessing.Pool(processes=N_CPUs) as pool:
         for extra in hilmo_diagnosis:
-            pool.apply_async(merge_extra_hilmo, args=(df, extra, outpath_hilmo))
+            pool.apply(merge_extra_hilmo, args=(df, extra, outpath_hilmo))
     pool.close()
-    pool.join()
     end_time = time.time()
     print(f'Time taken for {base_file}: {end_time - start_time} seconds')
