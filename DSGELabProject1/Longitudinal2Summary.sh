4eda9c6ec0caf210e4b4e1c8f7737f321815779d
ferroteo <ferro.teo98@gmail.com>
2025-02-24 09:17:17 +0200

    update to separate self visit in self-prescription and self-diagnosis

--- DSGELabProject1/Longitudinal2Summary.sh
+++ DSGELabProject1/Longitudinal2Summary.sh
@@ -2,8 +2,8 @@
 START_TIME=$(date +%s)
 
 # Input and output files
-INPUT_FILE="/media/volume/Projects/mattferr/doctor_patient_longitudinal_20250210.csv.gz"
-OUTPUT_FILE="/media/volume/Projects/mattferr/doctor_patient_summary_20250210.csv"
+INPUT_FILE="/media/volume/Projects/DSGELabProject1/doctor_patient_longitudinal_20250220.csv.gz"
+OUTPUT_FILE="/media/volume/Projects/DSGELabProject1/doctor_patient_summary_20250220.csv"
 
 # Run AWK to process the file
 zcat "$INPUT_FILE" | awk -F',' '
@@ -16,9 +16,14 @@ zcat "$INPUT_FILE" | awk -F',' '
         register_counts[$1][$3]++
     }
 
-    # Count self-prescriptions (DOCTOR_ID == PATIENT_ID)
-    if ($1 == $2) {
+    # Count self-prescriptions (DOCTOR_ID == PATIENT_ID and REGISTER is Prescription)
+    if (($1 == $2) && ($3 == "Prescription")) {
         self_prescriptions[$1]++
+    }
+
+    # Count self-diagnosis (DOCTOR_ID == PATIENT_ID and REGISTER is Diagnosis Avohilmo or Hilmo)
+    if (($1 == $2) && ($3 == "Diagnosis Avohilmo" || $3 == "Diagnosis Hilmo")) {
+        self_diagnosis[$1]++
     }
 
     # Count visits by sector
@@ -26,19 +31,20 @@ zcat "$INPUT_FILE" | awk -F',' '
     public_visits[$1] += $7
 }
 END {
-    print "DOCTOR_ID,TotalPatients,UniquePatients,Purchases,Prescriptions,DiagnosisAvohilmo,DiagnosisHilmo,SelfVisits,PrivatePrescriptions,PublicPrescriptions" > "'"$OUTPUT_FILE"'"  # Header
+    print "DOCTOR_ID,TotalPatients,UniquePatients,Purchases,Prescriptions,DiagnosisAvohilmo,DiagnosisHilmo,SelfPrescription,SelfDiagnosis,PrivatePrescriptions,PublicPrescriptions" > "'"$OUTPUT_FILE"'"  # Header
 
     for (doc in total_patients) {
         unique_count = length(unique_patients[doc])
         printf "%s,%d,%d", doc, total_patients[doc], unique_count > "'"$OUTPUT_FILE"'"
 
         # Print counts for each REGISTER type (default to 0 if missing)
-        printf ",%d,%d,%d,%d,%d,%d,%d\n", 
+        printf ",%d,%d,%d,%d,%d,%d,%d,%d\n", 
             register_counts[doc]["Purchase"], 
             register_counts[doc]["Prescription"], 
             register_counts[doc]["Diagnosis Avohilmo"], 
             register_counts[doc]["Diagnosis Hilmo"], 
             self_prescriptions[doc], 
+            self_diagnosis[doc],
             private_visits[doc], 
             public_visits[doc] > "'"$OUTPUT_FILE"'"
     }
