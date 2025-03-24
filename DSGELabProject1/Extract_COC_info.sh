5b3ca5444f0ff387efcd36529caa69b40a7fe4d7
ferroteo <ferro.teo98@gmail.com>
2025-03-24 08:22:54 +0200

    fix typo

--- DSGELabProject1/Extract_COC_info.sh
+++ DSGELabProject1/Extract_COC_info.sh
@@ -1,5 +1,5 @@
 #### Info
-# This script extracts the Box-Boxerman Continuity of Care (COC) information for each available patient in the dataset,
+# This script extracts the Bice-Boxerman Continuity of Care (COC) information for each available patient in the dataset,
 # starting from the output from Doctor_Patient_Longitudinal.py
 
 # Start timer
