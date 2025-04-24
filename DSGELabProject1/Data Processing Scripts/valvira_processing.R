5d9c096c192de3cc7b7e4e20ed4c1d04b2f34e60
ferroteo <ferro.teo98@gmail.com>
2025-04-24 13:39:12 +0300

    update to new rule

--- DSGELabProject1/Data Processing Scripts/valvira_processing.R
+++ DSGELabProject1/Data Processing Scripts/valvira_processing.R
@@ -1,3 +1,4 @@
+#### Load libraries 
 library(dplyr)
 library(tidyr)
 library(ggplot2)
@@ -10,10 +11,9 @@ library(stringr)
 library(data.table)
 library(R.utils)
 
-mpath <- "C:/Users/.../valvira/"
-setwd(mpath)
-
-valvira <- fread("valvira.csv", encoding = "Latin-1")
+#### Load data
+ValviraFile <- "/media/volume/Data/Data_THL_2698_14.02.00_2023/Valvira/FD_2698_Liite 1 THL_2698_14.02.00_2023.csv"
+valvira <- fread(ValviraFile, encoding = "Latin-1")
 
 glimpse(valvira)
 
@@ -21,18 +21,11 @@ valvira <- valvira %>% mutate(
     Ammattioikeus.voimassa.alkupäivämäärä = as.Date(Ammattioikeus.voimassa.alkupäivämäärä, format = "%d.%m.%Y"),
     Ammattioikeus.voimassa.loppupäivämäärä = as.Date(Ammattioikeus.voimassa.loppupäivämäärä, format = "%d.%m.%Y")
 )
-n_distinct(valvira$FID) 
-# Count of doctors: 46 097
+n_distinct(valvira$FID)
 
-# Criteria for filtering 1: have a practicing license
-# laillistettu erikoishammaslääkäri or laillistettu erikoislääkäri or laillistettu hammaslääkäri or
-# laillistettu lääkär or laillistettu lääkäri toisen johdon ja valvonnan alaisena or
-# laillistettu yleislääkäri
-
+# Criteria for filtering 1: have a practicing license & can prescribe all medications (remove dentists)
 valvira_licensed <- valvira %>% filter(
-    str_detect(Ammattioikeus, "laillistettu erikoishammaslääkäri") |
         str_detect(Ammattioikeus, "laillistettu erikoislääkäri") |
-        str_detect(Ammattioikeus, "laillistettu hammaslääkäri") |
         str_detect(Ammattioikeus, "laillistettu lääkär") |
         str_detect(Ammattioikeus, "laillistettu lääkäri toisen johdon ja valvonnan alaisena") |
         str_detect(Ammattioikeus, "laillistettu yleislääkäri")
@@ -40,15 +33,11 @@ valvira_licensed <- valvira %>% filter(
 
 glimpse(valvira_licensed)
 n_distinct(valvira_licensed$FID)
-# Count of doctors: 42 570
 
 # Criteria for filtering 2: have a practicing license for at least 5 years AND from 1998
-
 nrow(valvira_licensed %>% filter(is.na(Ammattioikeus.voimassa.alkupäivämäärä))) # 89 doctors without start date
 # remove the doctors without start date
 valvira_licensed <- valvira_licensed %>% filter(!is.na(Ammattioikeus.voimassa.alkupäivämäärä))
-# Count of doctors: 42 570 (stays the same because doctors had another active license)
-
 valvira_licensed_5years <- valvira_licensed %>%
     group_by(FID) %>%
     summarise(
@@ -88,7 +77,6 @@ calculate_practicing_days <- function(license_periods) {
     return(total_days)
 }
 
-
 valvira_licensed_5years <- valvira_licensed_5years %>%
     rowwise() %>%
     mutate(practicing_days = calculate_practicing_days(license_periods)) %>%
@@ -97,11 +85,11 @@ valvira_licensed_5years <- valvira_licensed_5years %>%
 valvira_licensed_5years <- valvira_licensed_5years %>%
     filter(practicing_days >= 1825) %>%
     filter(end_date >= as.Date("2003-01-01"))
-# Count of doctors: 37 126
+
+n_distinct(valvira_licensed_5years$FID)
 
 
 # Criteria for filtering 3: active doctor = treated (prescribed/diagnosed) at least 5 patients
-
 dp_summary <- fread("/media/volume/Projects/mattferr/doctor_patient_summary_20250218.csv")
 glimpse(dp_summary)
 
@@ -113,14 +101,11 @@ doctor_activity <- dp_summary %>%
     ) %>%
     select(FID = DOCTOR_ID, prescription_criteria, diagnosis_criteria, active_doctor)
 
-valvira_licensed_5years_active <- valvira_licensed_5years %>% left_join(doctor_activity, by = "FID") %>%
-    # filter(active_doctor == 1) # Count of doctors: 31 281
-    filter(prescription_criteria == 1)
-# Count of doctors: 30 995
-
+valvira_licensed_5years_active <- valvira_licensed_5years %>% left_join(doctor_activity, by = "FID") %>% filter(prescription_criteria == 1)
+n_distinct(valvira_licensed_5years_active$FID)
 
 # Criteria for filtering 4: doctor has BIRTHDATE and SEX information
-dvv <- fread("DVV/FD_2698_Tulokset_2024-04-09_HY.csv")
+dvv <- fread("/media/volume/Data/Data_THL_2698_14.02.00_2023/DVV/FD_2698_Tulokset_2024-04-09_HY.csv")
 # we know from data exploration that there is 0 missingness in these columns
 
 dvv <- dvv %>% select(FID, BIRTH_DATE = `Syntymä-päivä`, SEX = `Suku-.puoli`)
@@ -128,21 +113,17 @@ dvv <- dvv %>% select(FID, BIRTH_DATE = `Syntymä-päivä`, SEX = `Suku-.puoli`)
 valvira_licensed_5years_active <- valvira_licensed_5years_active %>%
     left_join(dvv, by = "FID") %>%
     filter(!is.na(BIRTH_DATE) & !is.na(SEX))
-# Count of doctors: 30 542
 
 # remove license periods column
 valvira_licensed_5years_active <- valvira_licensed_5years_active %>%
     select(-license_periods)
 
-# save dataset
-fwrite(valvira_licensed_5years_active, "/media/volume/Projects/DSGELabProject1/doctor_characteristics_woSpecialty_20250220.csv")
 # save only doctors 
 doctors <- valvira_licensed_5years_active %>% select(FID)
-fwrite(doctors, "/media/volume/Projects/DSGELabProject1/doctors_20250220.csv", col.names = FALSE)
+fwrite(doctors, "/media/volume/Projects/DSGELabProject1/doctors_20250424.csv", col.names = FALSE)
 
 
 #### Add last specialty information to each doctor ####
-
 # investigate the specialty information in the valvira dataset to find out whether there is a hierarchy in the specialty codes
 
 specialties <- valvira_licensed %>%
@@ -150,7 +131,6 @@ specialties <- valvira_licensed %>%
     group_by(Tutkinto_Koodi, Tutkinto) %>%
     summarise(count = n()) %>%
     arrange(Tutkinto_Koodi)
-
 
 specialties_count <- specialties %>%
     mutate(Tutkinto_Koodi_Prefix = ifelse(
@@ -165,7 +145,6 @@ specialties_count <- specialties %>%
 print(specialties)
 
 # create a dataframe with the doctor's longest specialty (trimmed to the numbers before the dash)
-
 longest_specialty <- valvira_licensed %>%
     mutate(
         Tutkinto_Koodi_Prefix = ifelse(
@@ -192,22 +171,16 @@ longest_specialty <- valvira_licensed %>%
 valvira_licensed_5years_active <- valvira_licensed_5years_active %>%
     left_join(longest_specialty, by = "FID")
 
-# save dataset
-fwrite(valvira_licensed_5years_active, "/media/volume/Projects/DSGELabProject1/doctor_characteristics_wlongest_Specialty_20250220.csv")
-
-
-
 # with specialty dictionary
-spec_dict  <- read_excel("/media/volume/Projects/jg/condensed_specialty_dict.xlsx", sheet = 1)
+spec_dict <- read.csv("/media/volume/Projects/DSGELabProject1/condensed_specialty_dict.csv")
 
 valvira_licensed <- valvira_licensed %>%
-    mutate(Tutkinto_Koodi = substr(Tutkinto_Koodi, 1, 5)) %>%
+    mutate(Tutkinto_Koodi = as.integer(substr(Tutkinto_Koodi, 1, 5))) %>%
     left_join(spec_dict %>% select(-COMPRISED), by = c("Tutkinto_Koodi" = "CODEVALUE"))
 
 # removing "NO SPECIALTY"
 valvira_only_special <- valvira_licensed %>%
-    mutate(LABEL_EN = ifelse(LABEL_EN %in%
-        c("LICENSED_MED", "LICENSED_DENT", "STUDENT_MED", "STUDENT_DENT"), NA, LABEL_EN)) %>% 
+    mutate(LABEL_EN = ifelse(LABEL_EN %in% c("LICENSED_MED", "LICENSED_DENT", "STUDENT_MED", "STUDENT_DENT"), NA, LABEL_EN)) %>% 
     filter(!is.na(LABEL_EN))
 
 # longest specialty w/o no specialty
@@ -218,16 +191,15 @@ last_specialty <- valvira_only_special %>%
     select(FID, LAST_SPECIALTY = LABEL_EN)
 
 valvira_characteristics <- valvira_licensed_5years_active %>%
+    mutate(SPECIALTY = is.integer(SPECIALTY)) %>%
     left_join(spec_dict %>% select(-COMPRISED), by = c("SPECIALTY" = "CODEVALUE")) %>%
     select(-SPECIALTY) %>% 
     dplyr::rename(LONGEST_SPECIALTY = LABEL_EN) %>% 
-    mutate(LONGEST_SPECIALTY = ifelse(LONGEST_SPECIALTY %in%
-        c("LICENSED_MED", "LICENSED_DENT", "STUDENT_MED", "STUDENT_DENT"), NA, LONGEST_SPECIALTY)) %>%
+    mutate(LONGEST_SPECIALTY = ifelse(LONGEST_SPECIALTY %in% c("LICENSED_MED", "LICENSED_DENT", "STUDENT_MED", "STUDENT_DENT"), NA, LONGEST_SPECIALTY)) %>%
     left_join(last_specialty, by = "FID")
 
 # QC this dataset
-valvira_characteristics <- valvira_characteristics %>%
-    mutate(BIRTH_DATE = ymd(BIRTH_DATE))
+valvira_characteristics <- valvira_characteristics %>% mutate(BIRTH_DATE = ymd(BIRTH_DATE))
 
 # save dataset
-fwrite(valvira_characteristics, "/media/volume/Projects/DSGELabProject1/doctor_characteristics_20250311.csv")
+fwrite(valvira_characteristics, "/media/volume/Projects/DSGELabProject1/doctor_characteristics_20250424.csv")
