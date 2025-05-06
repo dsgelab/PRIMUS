1da6e78745690dd3c2786aaf1aa107624498caf4
Mikael Lenander <mikael.lenander@infrakit.com>
2025-05-06 17:03:52 +0300

    Use only first diagnosis per patient in J06.9 stats, stacked diagnosis histogram by year and source dataset

--- DSGELabProject2/data-processing-scripts/upper_respiratory_summary.R
+++ DSGELabProject2/data-processing-scripts/upper_respiratory_summary.R
@@ -1,10 +1,21 @@
 library(data.table)
+library(dplyr)
+library(stringr)
+library(ggplot2)
 
 filename <- "/media/volume/Projects/DSGELabProject1/ProcessedData/AllConnectedDiagnosis_20250421.csv"
-diagnosis <- fread(filename)
-diagnosis <- diagnosis[startsWith(diagnosis$ICD10_CODE, "J06.9")]
+diagnosis <- fread(filename) %>% as_tibble()
+
+# Only select the earliest instance of diagnosis for each patient
+diagnosis <- diagnosis %>%
+  filter(str_starts(ICD10_CODE, "J06.9")) %>%
+  group_by(PATIENT_ID) %>%
+  arrange(DIAGNOSIS_DATE) %>%
+  slice(1) %>%
+  ungroup()
+
 count <- nrow(diagnosis)
-count_with_doctor <- nrow(diagnosis[!is.na(diagnosis$DOCTOR_ID)])
+count_with_doctor <- nrow(diagnosis %>% filter(!is.na(DOCTOR_ID)))
 percentage_with_doctor <- sprintf("%.2f%%", count_with_doctor / count * 100)
 print(paste("Number of upper respiratory diseases:", count))
 print(paste0("Number of upper respiratory diagnoses connected to a doctor: ", count_with_doctor, " (", percentage_with_doctor, ")"))
@@ -12,8 +23,14 @@ print(paste0("Number of upper respiratory diagnoses connected to a doctor: ", co
 codes <- unique(diagnosis$ICD10_CODE)
 print(paste("All ICD10 codes starting with J06.9:", codes))
 
-year_counts <- table(as.integer(format(diagnosis$DIAGNOSIS_DATE, "%Y")))
-barplot(year_counts,
-        main = "Number of J06.9 diagnoses per year",
-        xlab = "Year",
-        ylab = "Diagnoses")
+diagnosis %>%
+  mutate(DIAGNOSIS_YEAR = format(DIAGNOSIS_DATE, "%Y")) %>%
+  count(DIAGNOSIS_YEAR, SOURCE) %>%
+  ggplot(aes(x = DIAGNOSIS_YEAR, y = n, fill = SOURCE)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Number of J06.9 Diagnoses by Year and Source Dataset",
+       x = "Year",
+       y = "Diagnoses",
+       fill = "Source dataset") +
+  theme_minimal()
+
