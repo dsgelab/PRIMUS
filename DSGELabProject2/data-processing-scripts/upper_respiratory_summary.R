2b88fbf1109d3889b3516caa984285ef9b49d458
Mikael Lenander <mikael.lenander@infrakit.com>
2025-05-08 11:52:41 +0300

    Connect J06.9 diagnoses to prescriptions

--- DSGELabProject2/data-processing-scripts/upper_respiratory_summary.R
+++ DSGELabProject2/data-processing-scripts/upper_respiratory_summary.R
@@ -3,22 +3,26 @@ library(dplyr)
 library(stringr)
 library(ggplot2)
 
-filename <- "/media/volume/Projects/DSGELabProject1/ProcessedData/AllConnectedDiagnosis_20250421.csv"
-diagnosis <- fread(filename) %>% as_tibble()
+diagnosis_file <- "/media/volume/Projects/DSGELabProject1/ProcessedData/AllConnectedDiagnosis_20250421.csv"
+prescription_file <- "/media/volume/Projects/DSGELabProject1/ProcessedData/AllConnectedPrescriptions_20250506.csv"
 
+diagnosis <- fread(diagnosis_file) %>% as_tibble()
+diagnosis <- diagnosis %>%
+  filter(str_starts(ICD10_CODE, "J06.9"))
+print(paste("Number of total diagnoses", nrow(diagnosis)))
 # Only select the earliest instance of diagnosis for each patient
 diagnosis <- diagnosis %>%
-  filter(str_starts(ICD10_CODE, "J06.9")) %>%
   group_by(PATIENT_ID) %>%
   arrange(DIAGNOSIS_DATE) %>%
   slice(1) %>%
   ungroup()
 
 count <- nrow(diagnosis)
+print(paste("Number of first diagnoses:", count))
 count_with_doctor <- nrow(diagnosis %>% filter(!is.na(DOCTOR_ID)))
 percentage_with_doctor <- sprintf("%.2f%%", count_with_doctor / count * 100)
-print(paste("Number of upper respiratory diseases:", count))
-print(paste0("Number of upper respiratory diagnoses connected to a doctor: ", count_with_doctor, " (", percentage_with_doctor, ")"))
+print(paste0("Number of first diagnoses connected to a doctor: ", count_with_doctor, " (", percentage_with_doctor, ")"))
+write.csv(diagnosis, "J069Diagnoses_20250507.csv", row.names = FALSE)
 
 codes <- unique(diagnosis$ICD10_CODE)
 print(paste("All ICD10 codes starting with J06.9:", codes))
@@ -34,3 +38,20 @@ diagnosis %>%
        fill = "Source dataset") +
   theme_minimal()
 
+prescription <- fread(prescription_file) %>% as_tibble()
+
+# Pair diagnoses with prescriptions that have same (patient, doctor) pair
+pairs <- diagnosis %>%
+  inner_join(prescription, by = c("PATIENT_ID", "DOCTOR_ID"), suffix = c("_DIAG", "_PRES"))
+print(paste("Number of doctor patient pairs between diagnosis and prescription tables:", nrow(pairs)))
+write.csv(pairs, "DoctorPatientPairsWithJ069_20250507.csv", row.names = FALSE)
+
+# Select prescriptions that were made later the same week than the diagnosis or the next week
+diag_pres_pairs <- pairs %>%
+  filter(
+    PRESCRIPTION_DATE >= DIAGNOSIS_DATE & 
+      as.numeric(difftime(PRESCRIPTION_DATE, DIAGNOSIS_DATE, units = "days")) <= 
+      (7 - lubridate::wday(DIAGNOSIS_DATE, week_start = 1)) + 7
+  )
+print(paste("Number of doctor patient pairs where diagnosis (probably) led to a prescription:", nrow(diag_pres_pairs)))
+write.csv(diag_pres_pairs, "DiagnosesConnectedtoPrescriptions_J069_20250507.csv", row.names = FALSE)
