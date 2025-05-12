2758b304f465feee118b384be3faaf12c8d955a9
Mikael Lenander <mikael.lenander@infrakit.com>
2025-05-12 15:20:44 +0300

    J06.9 experiment 2: logistic regression

--- DSGELabProject2/data-processing-scripts/upper_respiratory_summary.R
+++ DSGELabProject2/data-processing-scripts/upper_respiratory_summary.R
@@ -8,14 +8,14 @@ prescription_file <- "/media/volume/Projects/DSGELabProject1/ProcessedData/AllCo
 
 diagnosis <- fread(diagnosis_file) %>% as_tibble()
 diagnosis <- diagnosis %>%
-  filter(str_starts(ICD10_CODE, "J06.9"))
+    filter(str_starts(ICD10_CODE, "J06.9"))
 print(paste("Number of total diagnoses", nrow(diagnosis)))
 # Only select the earliest instance of diagnosis for each patient
 diagnosis <- diagnosis %>%
-  group_by(PATIENT_ID) %>%
-  arrange(DIAGNOSIS_DATE) %>%
-  slice(1) %>%
-  ungroup()
+    group_by(PATIENT_ID) %>%
+    arrange(DIAGNOSIS_DATE) %>%
+    slice(1) %>%
+    ungroup()
 
 count <- nrow(diagnosis)
 print(paste("Number of first diagnoses:", count))
@@ -28,30 +28,52 @@ codes <- unique(diagnosis$ICD10_CODE)
 print(paste("All ICD10 codes starting with J06.9:", codes))
 
 diagnosis %>%
-  mutate(DIAGNOSIS_YEAR = format(DIAGNOSIS_DATE, "%Y")) %>%
-  count(DIAGNOSIS_YEAR, SOURCE) %>%
-  ggplot(aes(x = DIAGNOSIS_YEAR, y = n, fill = SOURCE)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Number of J06.9 Diagnoses by Year and Source Dataset",
-       x = "Year",
-       y = "Diagnoses",
-       fill = "Source dataset") +
-  theme_minimal()
+    mutate(DIAGNOSIS_YEAR = format(DIAGNOSIS_DATE, "%Y")) %>%
+    count(DIAGNOSIS_YEAR, SOURCE) %>%
+    ggplot(aes(x = DIAGNOSIS_YEAR, y = n, fill = SOURCE)) +
+    geom_bar(stat = "identity") +
+    labs(title = "Number of J06.9 Diagnoses by Year and Source Dataset",
+         x = "Year",
+         y = "Diagnoses",
+         fill = "Source dataset") +
+    theme_minimal()
 
 prescription <- fread(prescription_file) %>% as_tibble()
 
 # Pair diagnoses with prescriptions that have same (patient, doctor) pair
 pairs <- diagnosis %>%
-  inner_join(prescription, by = c("PATIENT_ID", "DOCTOR_ID"), suffix = c("_DIAG", "_PRES"))
+    inner_join(prescription, by = c("PATIENT_ID", "DOCTOR_ID"), suffix = c("_DIAG", "_PRES"))
 print(paste("Number of doctor patient pairs between diagnosis and prescription tables:", nrow(pairs)))
+# Only include pairs where the prescription is made after the diagnosis and
+# only the first diagnosis of each doctor-patient pair
+pairs <- pairs %>%
+    filter(PRESCRIPTION_DATE >= DIAGNOSIS_DATE)
+print(paste("Number of doctor-patient pairs where prescription is after diagnosis:", nrow(pairs)))
+pairs <- pairs %>%
+    group_by(PATIENT_ID, DOCTOR_ID) %>%
+    arrange(PRESCRIPTION_DATE) %>%
+    slice(1) %>%
+    ungroup()
+print(paste("Number of doctor-patient pairs where the prescription is the first after the diagnosis", nrow(pairs)))
 write.csv(pairs, "DoctorPatientPairsWithJ069_20250507.csv", row.names = FALSE)
+
+pairs <- pairs %>%
+    mutate(PRES_DIAG_DIFF = as.integer(difftime(PRESCRIPTION_DATE, DIAGNOSIS_DATE, units = "days")))
+
+ggplot(data.frame(x = pairs$PRES_DIAG_DIFF), aes(x)) +
+    geom_histogram(bins = 100) +
+    scale_x_continuous(n.breaks = 20) +
+    labs(title = "Histogram of Days from Diagnosis to Prescription",
+         x = "Days",
+         y = "Count") +
+    xlim(0, 365)
 
 # Select prescriptions that were made later the same week than the diagnosis or the next week
 diag_pres_pairs <- pairs %>%
-  filter(
-    PRESCRIPTION_DATE >= DIAGNOSIS_DATE & 
-      as.numeric(difftime(PRESCRIPTION_DATE, DIAGNOSIS_DATE, units = "days")) <= 
-      (7 - lubridate::wday(DIAGNOSIS_DATE, week_start = 1)) + 7
-  )
+    filter(
+        PRESCRIPTION_DATE >= DIAGNOSIS_DATE &
+            as.numeric(difftime(PRESCRIPTION_DATE, DIAGNOSIS_DATE, units = "days")) <=
+                (7 - lubridate::wday(DIAGNOSIS_DATE, week_start = 1)) + 7
+    )
 print(paste("Number of doctor patient pairs where diagnosis (probably) led to a prescription:", nrow(diag_pres_pairs)))
 write.csv(diag_pres_pairs, "DiagnosesConnectedtoPrescriptions_J069_20250507.csv", row.names = FALSE)
---
+++ DSGELabProject2/experiments/logistic_regression.R
@@ -0,0 +1,88 @@
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(pROC)
+library(ggplot2)
+
+set.seed(123)
+
+diagnosis_file = "/media/volume/Projects/mikael/J069Diagnoses_20250507.csv"
+prescription_file = "/media/volume/Projects/mikael/DiagnosesConnectedtoPrescriptions_J069_20250507.csv"
+doctor_file = "/media/volume/Projects/DSGELabProject1/doctor_characteristics_wlongest_Specialty_20250220.csv"
+patient_file = "/media/volume/Data/Data_THL_2698_14.02.00_2023/DVV/FD_2698_Tulokset_2024-04-09_HY.csv"
+
+diagnosis <- fread(diagnosis_file) %>% as_tibble()
+prescription <- fread(prescription_file) %>% as_tibble()
+doctor <- fread(doctor_file) %>% as_tibble()
+patient <- fread(patient_file) %>% as_tibble()
+
+# Dataset with doctor information
+df <- diagnosis %>%
+  inner_join(doctor, by = c("DOCTOR_ID" = "FID")) %>%
+  rename(D_BIRTH_DATE = BIRTH_DATE, D_SEX = SEX) %>%
+  mutate(D_BIRTH_DATE = ymd(D_BIRTH_DATE)) %>%
+  mutate(PRESCRIBED = as.integer(PATIENT_ID %in% prescription$PATIENT_ID)) %>%
+  mutate(D_AGE = as.numeric(difftime(DIAGNOSIS_DATE, D_BIRTH_DATE, units = "days") / 365.25))
+
+# Dataset with patient information
+df2 <- diagnosis %>%
+    inner_join(patient, by = c("PATIENT_ID" = "FID")) %>%
+    rename(P_BIRTH_DATE = `Syntymä-päivä`, P_SEX = `Suku-.puoli`) %>%
+    mutate(P_BIRTH_DATE = ymd(P_BIRTH_DATE)) %>%
+    mutate(PRESCRIBED = as.integer(PATIENT_ID %in% prescription$PATIENT_ID)) %>%
+    mutate(P_AGE = as.numeric(difftime(DIAGNOSIS_DATE, P_BIRTH_DATE, units = "days") / 365.25))
+
+
+dataset_size <- function(df, ...) {
+    clean_df_size = nrow(df %>% drop_na(...))
+    n_diagnoses = nrow(diagnosis)
+    df_diagnosis_ratio = sprintf("%.1f%%", clean_df_size / n_diagnoses * 100)
+    print(paste0("Dataset size: ", clean_df_size, ", ", df_diagnosis_ratio, " of diagnoses"))
+    prescribed = sum(df$PRESCRIBED)
+    prescribed_ratio = sprintf("%.1f%%", prescribed / clean_df_size * 100)
+    print(paste0("Number of patients with prescription: ", prescribed, ", ", prescribed_ratio, " of the dataset"))
+}
+dataset_size(df, D_BIRTH_DATE, D_SEX)
+
+# Account for imbalance in data
+model_weights <- function(df) {
+    class_counts = table(df$PRESCRIBED)
+    imbalance_ratio = round(max(class_counts) / min(class_counts))
+    w = ifelse(df$PRESCRIBED == 0, 1, imbalance_ratio)
+    w
+}
+
+weights <- model_weights(df)
+model <- glm(PRESCRIBED ~ D_AGE + D_SEX, data = df, family = "binomial", weights = weights)
+summary(model)
+
+regression_results <- function(model, df) {
+    probabilities = predict(model, type = "response")
+    predicted_class = ifelse(probabilities > 0.5, 1, 0)
+    conf_matrix = table(Predicted = predicted_class, Actual = df$PRESCRIBED)
+    precision = conf_matrix[2, 2] / (conf_matrix[2, 2] + conf_matrix[2, 1])
+    recall = conf_matrix[2, 2] / (conf_matrix[2, 2] + conf_matrix[1, 2])
+    accuracy = sum(diag(conf_matrix))/sum(conf_matrix)
+    print(paste("Precision:", precision, "Recall:", recall, "Accuracy:", accuracy))
+
+    conf_df = as.data.frame(as.table(conf_matrix))
+    ggplot(conf_df, aes(x = Actual, y = Predicted, fill = Freq)) +
+        geom_tile(color = "white") +
+        geom_text(aes(label = Freq), color = "white", size = 8) +
+        scale_fill_gradient(low = "lightblue", high = "darkblue") +
+        theme_minimal() +
+        theme(panel.grid = element_blank(),
+              axis.text = element_text(size = 12),
+              axis.title = element_text(size = 14)) +
+        labs(title = "Confusion Matrix",
+             subtitle = paste("Accuracy:", round(accuracy, 3))) +
+        coord_fixed()
+}
+regression_results(model, df)
+
+dataset_size(df2, P_AGE, P_SEX)
+weights2 <- model_weights(df2)
+model2 <- glm(PRESCRIBED ~ P_AGE + P_SEX, data = df2, family = "binomial", weights = weights2)
+summary(model2)
+regression_results(model2, df2)
