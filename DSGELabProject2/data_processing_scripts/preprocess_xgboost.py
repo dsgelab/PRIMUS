c0ed4c8da8e22c3eacf83fd8848f0b1676ca68b8
Mikael Lenander <mikael.lenander@infrakit.com>
2025-06-22 16:32:18 +0300

    XGBoost skeleton

--- DSGELabProject2/data_processing_scripts/preprocess_xgboost.py
+++ DSGELabProject2/data_processing_scripts/preprocess_xgboost.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import argparse
+from argparse_utils import probability
 from utils import find_latest_file_by_date
 
 
@@ -11,19 +14,37 @@ def fct_lump_min(series, min_count, other_label='other'):
     return series.replace(to_lump, other_label)
 
 
-df_file = find_latest_file_by_date("ProcessedData", r"J069DiagnosesWithPrescriptions_(\d{8}).csv")
-df = pd.read_csv(df_file)
+def preprocess_data(test_size):
+    df_file = find_latest_file_by_date("ProcessedData", r"J069DiagnosesWithPrescriptions_(\d{8}).csv")
+    df = pd.read_csv(df_file)
 
-sex_map = {1.0: "male", 2.0: "female"}
-df["SEX_DOC"] = df["SEX_DOC"].map(sex_map)
-df["SEX_PAT"] = df["SEX_PAT"].map(sex_map)
-df["VISIT_DATE"] = pd.to_datetime(df["VISIT_DATE"], format="%Y-%m-%d")
-df["SPECIALTY"] = df["SPECIALTY"].str.replace(" ", "_")
+    sex_map = {1.0: "male", 2.0: "female"}
+    df["SEX_DOC"] = df["SEX_DOC"].map(sex_map)
+    df["SEX_PAT"] = df["SEX_PAT"].map(sex_map)
+    df["VISIT_DATE"] = pd.to_datetime(df["VISIT_DATE"], format="%Y-%m-%d")
+    df["SPECIALTY"] = df["SPECIALTY"].str.replace(" ", "_")
 
-df["MONTH"] = df["VISIT_DATE"].dt.month
-df["WEEKDAY"] = df["VISIT_DATE"].dt.weekday
-df["LANGUAGE"] = fct_lump_min(df["LANGUAGE"], 10000)
-df = df[["SOURCE", "MONTH", "WEEKDAY", "SPECIALTY", "LANGUAGE", "AGE_DOC", "SEX_DOC", "BIRTH_REGION_DOC", "SEX_PAT", "HOME_REGION_PAT", "AGE_PAT", "PRESCRIBED"]]
-df = pd.get_dummies(df, columns=["SOURCE", "SPECIALTY", "LANGUAGE", "SEX_DOC", "BIRTH_REGION_DOC", "SEX_PAT", "HOME_REGION_PAT"])
+    df["MONTH"] = df["VISIT_DATE"].dt.month
+    df["WEEKDAY"] = df["VISIT_DATE"].dt.weekday
+    df["LANGUAGE"] = fct_lump_min(df["LANGUAGE"], 10000)
+    df = df[["SOURCE", "MONTH", "WEEKDAY", "SPECIALTY", "LANGUAGE", "AGE_DOC", "SEX_DOC", "BIRTH_REGION_DOC", "SEX_PAT", "HOME_REGION_PAT", "AGE_PAT", "PRESCRIBED"]]
+    df = pd.get_dummies(df, columns=["SOURCE", "SPECIALTY", "LANGUAGE", "SEX_DOC", "BIRTH_REGION_DOC", "SEX_PAT", "HOME_REGION_PAT"])
 
-df.to_csv("ProcessedData/xgboost_data.csv", index=False)
+    X, y = df.drop("PRESCRIBED", axis=1), df["PRESCRIBED"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42, stratify=y)
+    train_df = pd.concat([X_train, y_train], axis=1)
+    test_df = pd.concat([X_test, y_test], axis=1)
+    train_df.to_csv("ProcessedData/xgboost_train.csv", index=False)
+    test_df.to_csv("ProcessedData/xgboost_test.csv", index=False)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test_size", help="Test set size (0, 1)", type=probability, default=0.2)
+    args = parser.parse_args()
+
+    preprocess_data(args.test_size)
+
+
+if __name__ == "__main__":
+    main()
