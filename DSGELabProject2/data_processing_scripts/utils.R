9f12a6319f2e492e33e37b6a1b291ceca0d990ad
Mikael Lenander <mikael.lenander@infrakit.com>
2025-06-30 17:04:01 +0300

    Use Jacob's imputed prescriptions for J069 data

--- DSGELabProject2/data_processing_scripts/utils.R
+++ DSGELabProject2/data_processing_scripts/utils.R
@@ -3,7 +3,7 @@
 # Function to get the latest file path in cases where there are multiple files with the same name
 # but different dates, for example, diagnosis_20250505 and diagnosis_20250520.
 get_latest_file <- function(prefix) {
-    files <- list.files(pattern = paste0(prefix, "_[0-9]{8}\\.csv$"))
+    files <- list.files(pattern = paste0("^", prefix, "_[0-9]{8}\\.csv$"))
 
     if (length(files) == 0) {
         stop("No matching files found for prefix: ", prefix)
