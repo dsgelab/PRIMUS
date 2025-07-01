d271af9d865eba84f7d3239b54e03f35cc1122a4
Mikael Lenander <mikael.lenander@infrakit.com>
2025-07-01 23:32:51 +0300

    Fix join diagnoses script crashing when there are extra files in Avohilmo diagnoses folder

--- DSGELabProject1/Data Processing Scripts/join_diagnoses_and_procedures_to_base.py
+++ DSGELabProject1/Data Processing Scripts/join_diagnoses_and_procedures_to_base.py
@@ -1,6 +1,8 @@
 # Info
-# This script joins diagnosis and procedure files to the respective base files and saves them to two combined csv files
-# (connected diagnoses and connected procedures). Pipeline diagram in doc/AllConnectedPipeline.png.  The script takes roughly 9,5 hours to run.
+# This script joins diagnosis and procedure files to the respective base files (processed_{hilmo|avohilmo}_visits_YYYYMMDD.csv) and saves them to two
+# combined csv files (connected diagnoses and connected procedures). Pipeline diagram in doc/AllConnectedPipeline.png.  The script takes roughly 9,5
+# hours to run. Assumes that you have already run the join_doctors_to_base.py script that produces the base files.
+
 
 # Libraries
 import os
@@ -8,118 +10,126 @@ import pandas as pd
 import time
 import re
 from pathlib import Path
-from typing import NamedTuple
 from utils import find_latest_file_by_date, format_seconds_to_hms, count_csv_rows
 
-indir = Path("/media/volume/Projects/mikael/ProcessedData/")
-outdir = Path("/media/volume/Projects/mikael/ProcessedData/")
-avohilmo_diagnosis_path = Path("/media/volume/Data/Data_THL_2698_14.02.00_2023_pk1_kayntisyy_folk/THL/")
-THL_path = Path("/media/volume/Data/Data_THL_2698_14.02.00_2023/THL/")
-
-current_date = time.strftime("%Y%m%d")
+INDIR = Path("/media/volume/Projects/mikael/ProcessedData/")
+OUTDIR = Path("/media/volume/Projects/mikael/ProcessedData/")
+AVOHILMO_DIAGNOSIS_FOLDER = Path("/media/volume/Data/Data_THL_2698_14.02.00_2023_pk1_kayntisyy_folk/THL/")
+HILMO_DIAGNOSIS_FOLDER = Path("/media/volume/Data/Data_THL_2698_14.02.00_2023/THL/")
+AVOHILMO_PROCEDURE_FOLDER = Path("/media/volume/Data/Data_THL_2698_14.02.00_2023/THL/")
+HILMO_PROCEDURE_FOLDER = Path("/media/volume/Data/Data_THL_2698_14.02.00_2023/THL/")
 CHUNK_SIZE = 50_000_000
 
-DTYPES_HILMO_BASE = {"PATIENT_ID": "str", "HILMOID": "int", "VISIT_DATE": "str", "FD_HASH_CODE": "str", "DOCTOR_ID": "str"}
-DTYPES_AVOHILMO_BASE = {"PATIENT_ID": "str", "AVOHILMOID": "int", "VISIT_DATE": "str", "FD_HASH_CODE": "str", "DOCTOR_ID": "str"}
-DTYPES_AVOHILMO_PROCEDURES = {
-    "FID": "str",
-    "AVOHILMOID": "int",
-    "JARJESTYS": "int",
-    "TOIMENPIDE": "str",
-}
-DTYPES_HILMO_PROCEDURES = {
-    "FID": "str",
-    "HILMOID": "int",
-    "TOIMPALKUPVM": "str",
-    "N": "int",
-    "TOIMP": "str",
-}
-DTYPES_AVOHILMO_DIAGNOSES = {
-    "FID": "str",
-    "AVOHILMOID": "int",
-    "JARJESTYS": "int",
-    "LUOKITUS": "str",
-    "LUOKITUSLUOKKA": "str",
-}
-DTYPES_HILMO_DIAGNOSES = {
-    "FID": "str",
-    "HILMOID": "int",
-    "KOODI": "str",
-    "N": "int",
-    "KENTTA": "str",
-}
+current_date = time.strftime("%Y%m%d")
+ENCODING = "latin-1"
 
 
-class BaseDataset(NamedTuple):
+class BaseDataset:
     """
     Handles the base files (doctor visits) for both Avohilmo and Hilmo.
     """
 
-    label: str
-    dtypes: dict[str, str]
-    id_column: str
+    def __init__(self, label: str, id_column: str):
+        self.label = label
+        self.id_column = id_column
 
     @property
     def file_path(self):
-        return indir / find_latest_file_by_date(indir, rf"processed_{self.label.lower()}_visits_(\d{{8}}).csv")
+        return INDIR / find_latest_file_by_date(INDIR, rf"processed_{self.label.lower()}_visits_(\d{{8}}).csv")
+
+    @property
+    def dtypes(self):
+        return {
+            "PATIENT_ID": "str",
+            self.id_column: "int",
+            "VISIT_DATE": "str",
+            "FD_HASH_CODE": "str",
+            "DOCTOR_ID": "str",
+        }
 
 
-class OutputFile(NamedTuple):
+class OutputFile:
     """
     Represents a file to be produced by this script, for example, AllConnectedDiagnoses_YYYYMMDD.csv
     """
 
-    label: str
+    def __init__(self, label: str):
+        self.label = label
 
     @property
     def path(self):
-        return outdir / f"AllConnected{self.label}_{current_date}.csv"
+        return OUTDIR / f"AllConnected{self.label}_{current_date}.csv"
 
 
-class ProcedureDataset(NamedTuple):
+class JoinedDataset:
+    """
+    Contains common functionality for joined datasets. Not used directly.
+    """
+
+    def __init__(self, folder: Path, file_regex: str):
+        self.folder = folder
+        self.file_regex = file_regex
+
+    @property
+    def file_paths(self):
+        return tuple(self.folder / file for file in os.listdir(self.folder) if re.search(self.file_regex, file) and "~lock" not in file)
+
+
+class ProcedureDataset(JoinedDataset):
     """
     Handles the procedure files for both Avohilmo and Hilmo.
     """
 
-    file_regex: str
-    dtypes: dict[str, str]
-    procedure_column: str
-    new_code_column = "NOMESCO_CODE"
-    order_column: str
-
-    @property
-    def file_paths(self):
-        return tuple(THL_path / file for file in os.listdir(THL_path) if re.search(self.file_regex, file) and "~lock" not in file)
+    def __init__(self, folder: Path, file_regex: str, id_column: str, procedure_column: str, order_column: str):
+        super().__init__(folder, file_regex)
+        self.id_column = id_column
+        self.procedure_column = procedure_column
+        self.order_column = order_column
+        self.new_code_column = "NOMESCO_CODE"
 
     def preprocess(self, file_path):
         """
         Preprocesses a single procedure file (for example, FD2698_THL2023_H_TOIMENP_1.csv) and returns a dataframe.
         """
-        df = pd.read_csv(file_path, sep=";", encoding="latin1", usecols=self.dtypes.keys(), dtype=self.dtypes)
+        dtypes = {
+            "FID": "str",
+            self.id_column: "int",
+            self.order_column: "int",
+            self.procedure_column: "str",
+        }
+        df = pd.read_csv(file_path, sep=";", encoding=ENCODING, usecols=dtypes.keys(), dtype=dtypes)
         df = df[df[self.procedure_column].str.match(r"^[A-Z0-9]{5}$", na=False)]  # Filter nomesco codes
         df = df[df[self.order_column] == 0]  # Filter only main procedure
         df = df.rename(columns={self.procedure_column: self.new_code_column})
         return df
 
 
-class DiagnosisDataset(NamedTuple):
+class DiagnosisDataset(JoinedDataset):
     """
     Handles the diagnosis files for both Avohilmo and Hilmo.
     """
 
-    file_paths: tuple[Path]
-    dtypes: dict[str, str]
-    order_column: str
-    code_column: str
-    new_code_column = "ICD10_CODE"
-    class_column: str
-    class_value: str
+    def __init__(self, folder: Path, file_regex: str, id_column: str, order_column: str, code_column: str, class_column: str, class_value: str):
+        super().__init__(folder, file_regex)
+        self.id_column = id_column
+        self.order_column = order_column
+        self.code_column = code_column
+        self.class_column = class_column
+        self.class_value = class_value
+        self.new_code_column = "ICD10_CODE"
 
     def preprocess(self, file_path):
         """
         Preprocesses a single diagnosis file (for example, FD_2698_THL2023_2698_H_ICD10_1.csv) and returns a dataframe.
         """
-        df = pd.read_csv(file_path, sep=";", encoding="latin-1", usecols=self.dtypes.keys(), dtype=self.dtypes)
+        dtypes = {
+            "FID": "str",
+            self.id_column: "int",
+            self.order_column: "int",
+            self.code_column: "str",
+            self.class_column: "str",
+        }
+        df = pd.read_csv(file_path, sep=";", encoding=ENCODING, usecols=dtypes.keys(), dtype=dtypes)
         # filter only first and main diagnosis
         df = df[(df[self.order_column] == 0) & (df[self.class_column] == self.class_value)]
         df = df.rename(columns={self.code_column: self.new_code_column})
@@ -133,30 +143,34 @@ def DatasetFactory(output_file, base_dataset):
     """
     registry = {
         ("Diagnoses", "Avohilmo"): DiagnosisDataset(
-            file_paths=[avohilmo_diagnosis_path / file for file in os.listdir(avohilmo_diagnosis_path) if "~lock" not in file],
-            dtypes=DTYPES_AVOHILMO_DIAGNOSES,
+            folder=AVOHILMO_DIAGNOSIS_FOLDER,
+            file_regex=r"AH_KAYNTISYY_\d",
+            id_column="AVOHILMOID",
             order_column="JARJESTYS",
             code_column="LUOKITUS",
             class_column="LUOKITUSLUOKKA",
             class_value="icd10",
         ),
         ("Diagnoses", "Hilmo"): DiagnosisDataset(
-            file_paths=[THL_path / file for file in os.listdir(THL_path) if re.search(r"H_ICD10_\d", file) and "~lock" not in file],
-            dtypes=DTYPES_HILMO_DIAGNOSES,
+            folder=HILMO_DIAGNOSIS_FOLDER,
+            file_regex=r"H_ICD10_\d",
+            id_column="HILMOID",
             order_column="N",
             code_column="KOODI",
             class_column="KENTTA",
             class_value="PDGO",
         ),
         ("Procedures", "Avohilmo"): ProcedureDataset(
+            folder=AVOHILMO_PROCEDURE_FOLDER,
             file_regex=r"AH_TOIMENPITEET_\d",
-            dtypes=DTYPES_AVOHILMO_PROCEDURES,
+            id_column="AVOHILMOID",
             procedure_column="TOIMENPIDE",
             order_column="JARJESTYS",
         ),
         ("Procedures", "Hilmo"): ProcedureDataset(
+            folder=HILMO_PROCEDURE_FOLDER,
             file_regex=r"H_TOIMENP_\d",
-            dtypes=DTYPES_HILMO_PROCEDURES,
+            id_column="HILMOID",
             procedure_column="TOIMP",
             order_column="N",
         ),
@@ -168,8 +182,8 @@ def DatasetFactory(output_file, base_dataset):
 output_files = (OutputFile(label="Diagnoses"), OutputFile(label="Procedures"))
 # Specifies the base datasets to be used by the script. Comment out the base datasets you don't need to include.
 base_datasets = (
-    BaseDataset(label="Avohilmo", dtypes=DTYPES_AVOHILMO_BASE, id_column="AVOHILMOID"),
-    BaseDataset(label="Hilmo", dtypes=DTYPES_HILMO_BASE, id_column="HILMOID"),
+    BaseDataset(label="Avohilmo", id_column="AVOHILMOID"),
+    BaseDataset(label="Hilmo", id_column="HILMOID"),
 )
 
 
@@ -190,7 +204,7 @@ if __name__ == "__main__":
                 base_dataset.file_path,
                 chunksize=CHUNK_SIZE,
                 sep=",",
-                encoding="latin-1",
+                encoding=ENCODING,
                 usecols=base_dataset.dtypes.keys(),
                 dtype=base_dataset.dtypes,
             ):  # The base dataset (processed_{hilmo|avohilmo}_visits_YYYYMMDD.csv) may contain 1,000,000,000 rows, so we read it in chunks
@@ -202,8 +216,8 @@ if __name__ == "__main__":
                     joined_chunk = joined_dataset.preprocess(file_path)
                     df = pd.merge(base_chunk, joined_chunk, on=base_dataset.id_column, how="inner")
                     df = df[["PATIENT_ID", "VISIT_DATE", joined_dataset.new_code_column, "SOURCE", "FD_HASH_CODE", "DOCTOR_ID"]]
-                    df.to_csv(output_file.path, mode="a", header=not output_file.path.exists(), encoding="latin-1", index=False)
-                    rows = f"{base_chunk.index[0]+1}-{base_chunk.index[-1]+1} of {total_rows}"
+                    df.to_csv(output_file.path, mode="a", header=not output_file.path.exists(), encoding=ENCODING, index=False)
+                    rows = f"{base_chunk.index[0] + 1}-{base_chunk.index[-1] + 1} of {total_rows}"
                     print(f"Joined {file_path.name} to {base_dataset.label} base rows {rows} and saved to {output_file.path.name}")
 
     print(f"\nFinished processing all files in {format_seconds_to_hms(time.time() - script_start)}.")
