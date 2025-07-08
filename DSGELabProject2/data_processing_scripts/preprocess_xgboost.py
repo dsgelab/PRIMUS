8557e5a2ee2dfd64f07bde526acb543b9e8aba32
Mikael Lenander <mikael.lenander1@gmail.com>
2025-07-08 21:25:34 +0300

    Add patient/doctor prescription/diagnosis history to J069 model as binary features

--- DSGELabProject2/data_processing_scripts/preprocess_xgboost.py
+++ DSGELabProject2/data_processing_scripts/preprocess_xgboost.py
@@ -2,7 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import argparse
 from argparse_utils import probability
-from utils import find_latest_file_by_date
+from utils import find_latest_file_by_date, format_seconds_to_hms
+import time
 
 
 def fct_lump_min(series, min_count, other_label="other"):
@@ -22,8 +23,9 @@ def split_train_test_df(df, test_size):
     return train_df, test_df
 
 
-def preprocess_data(test_size, categorical_encoding, outdir):
-    df_file = find_latest_file_by_date("ProcessedData", r"J069DiagnosesWithPrescriptions_(\d{8}).csv")
+def preprocess_data(test_size, categorical_encoding, outdir, user_suffix, input_file):
+    df_file = find_latest_file_by_date("ProcessedData", rf"{input_file}_(\d{{8}})\.csv")
+    print(f"Using {df_file}")
     df = pd.read_csv(df_file)
 
     sex_map = {1.0: "male", 2.0: "female"}
@@ -35,6 +37,10 @@ def preprocess_data(test_size, categorical_encoding, outdir):
     df["MONTH"] = df["VISIT_DATE"].dt.month
     df["WEEKDAY"] = df["VISIT_DATE"].dt.weekday
     df["LANGUAGE_DOC"] = fct_lump_min(df["LANGUAGE_DOC"], 10000)
+    disease_history_cols = df.columns[df.columns.str.startswith("HAD_")].tolist()
+    medication_history_cols = df.columns[df.columns.str.startswith("GOT_")].tolist()
+    print("disease_history_cols: ", len(disease_history_cols))
+    print("medication_history_cols: ", len(medication_history_cols))
     df = df[
         [
             "MONTH",
@@ -48,6 +54,8 @@ def preprocess_data(test_size, categorical_encoding, outdir):
             "HOME_REGION_PAT",
             "AGE_PAT",
             "PRESCRIBED",
+            *disease_history_cols,
+            *medication_history_cols,
         ]
     ]
     categorical_features = ["SPECIALTY", "LANGUAGE_DOC", "SEX_DOC", "HOME_REGION_DOC", "SEX_PAT", "HOME_REGION_PAT"]
@@ -63,20 +71,31 @@ def preprocess_data(test_size, categorical_encoding, outdir):
             test_df[key] = test_df[key].map(freq_maps[key])
         filename_suffix = "_freq"
 
-    train_df.to_csv(f"{outdir}/xgboost_train{filename_suffix}.csv", index=False)
-    test_df.to_csv(f"{outdir}/xgboost_test{filename_suffix}.csv", index=False)
+    train_df.to_csv(f"{outdir}/xgboost_train{filename_suffix}{user_suffix}.csv", index=False)
+    test_df.to_csv(f"{outdir}/xgboost_test{filename_suffix}{user_suffix}.csv", index=False)
 
 
 def main():
+    start_time = time.time()
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--outdir", help="Path to the output directory (default=./).", type=str, default="./")
-    parser.add_argument("--test_size", help="Test set size as a decimal (default=0.2).", type=probability, default=0.2)
+    parser.add_argument("--testsize", help="Test set size as a decimal (default=0.2).", type=probability, default=0.2)
     parser.add_argument(
         "--cat-encoding", help="Encoding method for categorical features (default=one_hot).", type=str, choices=["one_hot", "freq"], default="one_hot"
     )
+    parser.add_argument("--suffix", help="Suffix for output files (default='').", type=str, default="")
+    parser.add_argument(
+        "--inputfile",
+        help="Path pattern to the file containing training samples (see utils.py::find_latest_file_by_date).",
+        type=str,
+        default="J069DiagnosesWithPrescriptions",
+    )
     args = parser.parse_args()
 
-    preprocess_data(args.test_size, args.cat_encoding, args.outdir)
+    preprocess_data(args.testsize, args.cat_encoding, args.outdir, args.suffix, args.inputfile)
+
+    print(f"Script finished in {format_seconds_to_hms(time.time() - start_time)}.")
 
 
 if __name__ == "__main__":
