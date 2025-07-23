cebe6efaf0176bf8070649c8a7db50441b070f3b
Mikael Lenander <mikael.lenander1@gmail.com>
2025-07-23 22:06:25 +0300

    Set aut omode for shapdsize argument of train_xgboost.py

--- DSGELabProject2/data_processing_scripts/argparse_utils.py
+++ DSGELabProject2/data_processing_scripts/argparse_utils.py
@@ -1,5 +1,7 @@
 import argparse
 
+
+# Accepts float in [0,1]
 def probability(x):
     try:
         x = float(x)
@@ -8,3 +10,10 @@ def probability(x):
     if x < 0 or x > 1:
         raise argparse.ArgumentTypeError("Must be between 0 and 1")
     return x
+
+
+# Accepts float in [0,1] or the string 'auto'
+def probability_or_auto(x):
+    if x == "auto":
+        return x
+    return probability(x)
