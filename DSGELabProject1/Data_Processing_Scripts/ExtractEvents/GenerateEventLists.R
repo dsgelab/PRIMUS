91145cf73c70af9604dd061c722b350cf62610a0
ferroteo <ferro.teo98@gmail.com>
2026-01-21 10:08:39 +0200

    add old atc codes to list

--- DSGELabProject1/Data_Processing_Scripts/ExtractEvents/GenerateEventLists.R
+++ DSGELabProject1/Data_Processing_Scripts/ExtractEvents/GenerateEventLists.R
@@ -4,6 +4,7 @@ suppressPackageStartupMessages({
 })
 
 # Configuration
+renamed_ATC_file <- "/media/volume/Projects/ATC_renamed_codes.csv"
 INPUT_DIR <- "/media/volume/Projects/DSGELabProject1/ProcessedData/Pairs_20251028/"  
 OUTPUT_DIR <- INPUT_DIR
 
@@ -60,13 +61,24 @@ outfile = file.path("/media/volume/Projects/DSGELabProject1/DiD_Diagnosis/event_
 fwrite(icd_filtered[, .(NewCode)], outfile, col.names = FALSE, quote = FALSE)
 
 # Generate event and event-outcome pair ATC codes for DiD analysis
+# Check renamed ATC codes and fix accordingly
+renamed_atc <- fread(renamed_ATC_file)
 event_output <- file.path("/media/volume/Projects/DSGELabProject1/DiD_Medications/event_codes_ATC_20251028.csv")
+outcome_output <- file.path("/media/volume/Projects/DSGELabProject1/DiD_Medications/outcome_codes_ATC_20251028.csv")
 pairs_output <- file.path("/media/volume/Projects/DSGELabProject1/DiD_Medications/event_outcome_pairs_ATC_20251028.csv")
-atc_events <- data.table(event = paste0("Purch_", atc_filtered$CODE))
+
+old_codes <- renamed_atc[renamed_atc$ATC_NEW %in% atc_filtered$CODE, .(ATC_NEW, ATC_OLD)]
+atc_filtered_new <- rbind(atc_filtered, data.table(CODE = old_codes$ATC_OLD, UNIQUE_ID_COUNT = NA), fill = TRUE)
+atc_outcome <- atc_filtered_new[,.(CODE)]
+cat("Adding old ATC codes to the list...\n")
+cat("Number of old codes added to list: ", nrow(old_codes), "\n")
+atc_events <- data.table(event = paste0("Purch_", atc_filtered_new$CODE))
+
+fwrite(atc_outcome, outcome_output, col.names = FALSE, quote = FALSE)
 fwrite(atc_events, event_output, col.names = FALSE, quote = FALSE)
 atc_pairs <- data.table(
-    event = paste0("Purch_", atc_filtered$CODE),
-    outcome = atc_filtered$CODE
+    event = paste0("Purch_", atc_filtered_new$CODE),
+    outcome = atc_filtered_new$CODE
 )
 fwrite(atc_pairs, pairs_output, col.names = FALSE, quote = FALSE)
 
@@ -74,7 +86,12 @@ fwrite(atc_pairs, pairs_output, col.names = FALSE, quote = FALSE)
 # Summary
 cat("\n================================================\n")
 cat("Filtering complete!\n")
-cat(sprintf("Minimum count threshold: %d\n", THRESHOLD))
 cat(sprintf("ATC output: %s\n", atc_output))
 cat(sprintf("ICD output: %s\n", icd_output))
+cat(sprintf("Minimum count threshold: %d\n", THRESHOLD))
+cat("Generated DiD analysis code lists:\n")
+cat(sprintf("ICD diagnosis output: %s\n", outfile))
+cat(sprintf("ATC event output: %s\n", event_output))
+cat(sprintf("ATC outcome output: %s\n", outcome_output))
+cat(sprintf("ATC pairs output: %s\n", pairs_output))
 cat("================================================\n")
